99f9620c49f9cb0a8144fbc77807c32a6b1cc8f7
rayzhouzhj <ray.zhou.zhj@gmail.com>
2018-02-02 00:00:38 +0800

    Add back TestLogger and make it as element of basetest

--- src/main/java/com/github/testcommon/test/base/BaseTest.java
+++ src/main/java/com/github/testcommon/test/base/BaseTest.java
@@ -2,12 +2,10 @@ package com.github.testcommon.test.base;
 
 import java.util.Random;
 
-import org.testng.Assert;
-
-import com.github.framework.report.ReportManager;
-
 public class BaseTest
 {
+	protected TestLogger logger = new TestLogger();
+	
 	public String getRandomNumberString(int length)
 	{
 		String output = "";
@@ -32,35 +30,5 @@ public class BaseTest
 		{
 			e.printStackTrace();
 		}
-	}
-	
-	public void logInfo(String message)
-	{
-		System.out.println("[INFO] " + message);
-		ReportManager.getInstance().logInfo(message);
-	}
-	
-	public void logInfoWithScreenShot(String message)
-	{
-		System.out.println("[INFO] " + message);
-		ReportManager.getInstance().logInfoWithScreenShot(message);
-	}
-	
-	public void logPass(String message)
-	{
-		System.out.println("[PASSED] " + message);
-		ReportManager.getInstance().logPass(message);
-	}
-	
-	public void logFail(String message)
-	{
-		System.err.println("[FAILED] " + message);
-		ReportManager.getInstance().logFail(message);
-	}
-	
-	public void logFatalError(String message)
-	{
-		System.err.println("[ERROR] " + message);
-		Assert.fail(message);
 	}
 }
